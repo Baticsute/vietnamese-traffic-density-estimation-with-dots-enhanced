3bd4c789ab601a47bd04c681668d461c14bfcda6
lbbduy <baticsute@gmail.com>
2022-07-29 02:42:57 +0700

    Tensorflow gpu unet

--- train_test.py
+++ train_test.py
@@ -93,9 +93,15 @@ def prepare_mask_data(path):
 
     return data
 
+
 X_train = prepare_image_data(TRAIN_PATH_IMAGES)
 Y_train = prepare_mask_data(TRAIN_PATH_MASKS)
 
+X_test = prepare_image_data(TEST_PATH_IMAGES)
+Y_test = prepare_mask_data(TEST_PATH_MASKS)
+
+X_val = prepare_image_data(VALI_PATH_IMAGES)
+Y_val = prepare_mask_data(VALI_PATH_MASKS)
 
 # Check if training data looks all right
 # ix = random.randint(0, len(train_ids))
@@ -105,76 +111,75 @@ Y_train = prepare_mask_data(TRAIN_PATH_MASKS)
 # plt.show()
 
 inputs = Input((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
-s = Lambda(lambda x: x / 255) (inputs)
+s = Lambda(lambda x: x / 255)(inputs)
 
-c1 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (s)
+c1 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(s)
 c1 = BatchNormalization()(c1)
-c1 = Dropout(0.1) (c1)
-c1 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (c1)
+c1 = Dropout(0.1)(c1)
+c1 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
 c1 = BatchNormalization()(c1)
-p1 = MaxPooling2D((2, 2)) (c1)
+p1 = MaxPooling2D((2, 2))(c1)
 
-c2 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (p1)
+c2 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p1)
 c2 = BatchNormalization()(c2)
-c2 = Dropout(0.1) (c2)
-c2 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (c2)
+c2 = Dropout(0.1)(c2)
+c2 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
 c2 = BatchNormalization()(c2)
-p2 = MaxPooling2D((2, 2)) (c2)
+p2 = MaxPooling2D((2, 2))(c2)
 
-c3 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (p2)
+c3 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p2)
 c3 = BatchNormalization()(c3)
-c3 = Dropout(0.2) (c3)
-c3 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (c3)
+c3 = Dropout(0.2)(c3)
+c3 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
 c3 = BatchNormalization()(c3)
-p3 = MaxPooling2D((2, 2)) (c3)
+p3 = MaxPooling2D((2, 2))(c3)
 
-c4 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (p3)
+c4 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p3)
 c4 = BatchNormalization()(c4)
-c4 = Dropout(0.2) (c4)
-c4 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (c4)
+c4 = Dropout(0.2)(c4)
+c4 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
 c4 = BatchNormalization()(c4)
-p4 = MaxPooling2D(pool_size=(2, 2)) (c4)
+p4 = MaxPooling2D(pool_size=(2, 2))(c4)
 
-c5 = Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (p4)
+c5 = Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p4)
 c5 = BatchNormalization()(c5)
-c5 = Dropout(0.3) (c5)
-c5 = Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (c5)
+c5 = Dropout(0.3)(c5)
+c5 = Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
 c5 = BatchNormalization()(c5)
 
-u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same') (c5)
+u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c5)
 u6 = concatenate([u6, c4])
-c6 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (u6)
+c6 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u6)
 c6 = BatchNormalization()(c6)
-c6 = Dropout(0.2) (c6)
-c6 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (c6)
+c6 = Dropout(0.2)(c6)
+c6 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c6)
 c6 = BatchNormalization()(c6)
 
-u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same') (c6)
+u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c6)
 u7 = concatenate([u7, c3])
-c7 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (u7)
+c7 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u7)
 c7 = BatchNormalization()(c7)
-c7 = Dropout(0.2) (c7)
-c7 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (c7)
+c7 = Dropout(0.2)(c7)
+c7 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c7)
 c7 = BatchNormalization()(c7)
 
-u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same') (c7)
+u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c7)
 u8 = concatenate([u8, c2])
-c8 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (u8)
+c8 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u8)
 c8 = BatchNormalization()(c8)
-c8 = Dropout(0.1) (c8)
-c8 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (c8)
+c8 = Dropout(0.1)(c8)
+c8 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c8)
 c8 = BatchNormalization()(c8)
 
-u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same') (c8)
+u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c8)
 u9 = concatenate([u9, c1], axis=3)
-c9 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (u9)
+c9 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u9)
 c9 = BatchNormalization()(c9)
-c9 = Dropout(0.1) (c9)
-c9 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (c9)
+c9 = Dropout(0.1)(c9)
+c9 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c9)
 c9 = BatchNormalization()(c9)
 
-outputs = Conv2D(1, (1, 1), activation='sigmoid') (c9)
-
+outputs = Conv2D(1, (1, 1), activation='sigmoid')(c9)
 
 model = Model(inputs=[inputs], outputs=[outputs])
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
@@ -182,5 +187,11 @@ model.summary()
 
 earlystopper = EarlyStopping(patience=3, verbose=1)
 checkpointer = ModelCheckpoint('model_unet_checkpoint.h5', verbose=1, save_best_only=True)
-results = model.fit(X_train, Y_train, validation_split=0.0, batch_size=1, epochs=10,
-                    callbacks=[earlystopper, checkpointer])
+results = model.fit(
+    X_train,
+    Y_train,
+    validation_data=(X_val, Y_val),
+    batch_size=128,
+    epochs=200,
+    callbacks=[earlystopper, checkpointer]
+)
