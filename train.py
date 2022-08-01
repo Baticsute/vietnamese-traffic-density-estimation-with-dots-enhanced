88f51c08db6897b8389a0e066bd08335bf5f0bd9
lbbduy <baticsute@gmail.com>
2022-08-02 01:59:24 +0700

    Tensorflow gpu unet

--- train.py
+++ train.py
@@ -1,32 +1,6 @@
-import os
-import sys
-import pathlib
-import random
-
-import numpy as np
-import pandas as pd
-
 import matplotlib.pyplot as plt
-from tqdm import tqdm
-from itertools import chain
-from skimage.io import imread, imshow, imread_collection, concatenate_images
-from skimage.transform import resize
-from skimage.morphology import label
-from PIL import ImageFile
-
-from tensorflow.keras.models import Model, load_model
-from tensorflow.keras.layers import Input
-from tensorflow.keras.layers import Dropout, Lambda
-from tensorflow.keras.layers import Conv2D, Conv2DTranspose, BatchNormalization
-from tensorflow.keras.layers import MaxPooling2D
-from tensorflow.keras.layers import concatenate
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
-from tensorflow.keras import backend as K
 
 from utils import data_loader
-from utils import unet_model
-
-import tensorflow as tf
 
 from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
@@ -36,11 +10,22 @@ config.gpu_options.allow_growth = True
 session = InteractiveSession(config=config)
 
 
-train_data = data_loader.load_train_data('final_data')
-test_data = data_loader.load_test_data('final_data')
+train_data = data_loader.load_train_data('final_data', is_dots_expanded=True, expand_size=2)
+test_data = data_loader.load_test_data('final_data', is_dots_expanded=True, expand_size=2)
 
-unet = unet_model.get_unet_model(img_h=96, img_w=128, img_ch=1)
-unet_model.train_model(model=unet, train_data=train_data, batch_size=8, n_epochs=10)
+s1 = train_data['train_data'][0]
+l1 = train_data['train_label_data'][0]
+
+plt.figure(figsize=(30, 15))
+plt.imshow(s1)
+plt.show()
+
+plt.figure(figsize=(30, 15))
+plt.imshow(l1)
+plt.show()
+
+# unet = unet_model.get_unet_model(img_h=96, img_w=128, img_ch=1)
+# unet_model.train_model(model=unet, train_data=train_data, batch_size=8, n_epochs=10)
 
 # Check if training data looks all right
 # ix = random.randint(0, len(train_ids))
--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,15 +1,13 @@
 import os
 import sys
 import pathlib
-import random
 
 import numpy as np
 
 from tqdm import tqdm
-from skimage.io import imread, imshow, imread_collection, concatenate_images
-from skimage.util import img_as_float
+from skimage.io import imread
 from skimage.transform import resize
-from PIL import ImageFile
+from skimage.segmentation import expand_labels
 
 ROOT_PATH = str(pathlib.Path().absolute())
 DATA_STORAGE_PATH = '/data_storage/'
@@ -49,12 +47,14 @@ def prepare_mask_data(path, save_file_name, dataset_name, img_h=96, img_w=128):
 
     file_ids = next(os.walk(path))[2]
     data = np.zeros((len(file_ids), img_h, img_w, 1), dtype=np.bool)
+    mask_sizes = []
 
     for n, id_ in tqdm(enumerate(file_ids), total=len(file_ids)):
         # Read corresponding mask files iteratively
         mask = np.zeros((img_h, img_w, 1), dtype=np.bool)
 
         mask_ = imread(path + id_)
+        mask_sizes.append([mask.shape[0], mask.shape[1]])
         # Expand individual mask dimensions
         mask_ = np.expand_dims(resize(mask_, (img_h, img_w), mode='constant', preserve_range=True), axis=-1)
 
@@ -65,17 +65,22 @@ def prepare_mask_data(path, save_file_name, dataset_name, img_h=96, img_w=128):
     np.save(save_path, data)
     print("{0}.npy has been saved at {1} ".format(save_file_name, STORAGE_PATH + dataset_name))
 
+    np.save(save_path + '_size', mask_sizes, allow_pickle=True)
+    print("{0}.npy has been saved at {1} ".format(save_file_name + '_size', STORAGE_PATH + dataset_name))
 
 def get_train_data(dataset_name, img_h=96, img_w=128, img_ch=1):
     prepare_image_data(TRAIN_PATH_IMAGES, 'X_train', dataset_name, img_h=img_h, img_w=img_w, img_ch=img_ch)
-    prepare_mask_data(TRAIN_PATH_MASKS, 'Y_train', dataset_name, img_h=img_h, img_w=img_h)
+    prepare_mask_data(TRAIN_PATH_MASKS, 'Y_train', dataset_name, img_h=img_h, img_w=img_w)
 
 
-def load_train_data(dataset_name):
+def load_train_data(dataset_name, is_dots_expanded=True, expand_size=1):
     file_path = STORAGE_PATH + dataset_name
 
     X_train = np.load(file_path + '/' + 'X_train.npy')
     Y_train = np.load(file_path + '/' + 'Y_train.npy')
+
+    if is_dots_expanded:
+        Y_train = expand_labels(Y_train, distance=expand_size)
 
     return {
         'train_data': X_train,
@@ -88,11 +93,14 @@ def get_test_data(dataset_name, img_h=96, img_w=128, img_ch=1):
     prepare_mask_data(TEST_PATH_MASKS, 'Y_test', dataset_name, img_h=img_h, img_w=img_w)
 
 
-def load_test_data(dataset_name):
+def load_test_data(dataset_name, is_dots_expanded=True, expand_size=1):
     file_path = STORAGE_PATH + dataset_name
 
     X_test = np.load(file_path + '/' + 'X_test.npy')
     Y_test = np.load(file_path + '/' + 'Y_test.npy')
+
+    if is_dots_expanded:
+        Y_test = expand_labels(Y_test, distance=expand_size)
 
     return {
         'test_data': X_test,
@@ -104,11 +112,14 @@ def get_validation_data(dataset_name, img_h=96, img_w=128, img_ch=1):
     prepare_image_data(VALI_PATH_IMAGES, 'X_val', dataset_name, img_h=img_h, img_w=img_w, img_ch=img_ch)
     prepare_mask_data(VALI_PATH_MASKS, 'Y_val', dataset_name, img_h=img_h, img_w=img_w)
 
-def load_val_data(dataset_name):
+def load_val_data(dataset_name, is_dots_expanded=True, expand_size=1):
     file_path = STORAGE_PATH + dataset_name
 
     X_val = np.load(file_path + '/' + 'X_val.npy')
     Y_val = np.load(file_path + '/' + 'Y_val.npy')
+
+    if is_dots_expanded:
+        Y_val = expand_labels(Y_val, distance=expand_size)
 
     return {
         'val_data': X_val,
