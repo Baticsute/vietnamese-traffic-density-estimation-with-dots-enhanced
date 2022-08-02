53d305c4535fa60e667c481fc955f0f7c359a63d
lbbduy <lbbduy@ninepoints.vn>
2022-08-03 01:36:46 +0900

    - Data loader update

--- generate_data.py
+++ generate_data.py
@@ -1,4 +1,22 @@
 from utils import data_loader
+import pathlib
 
-data_loader.get_train_data('final_data', img_h=96, img_w=128, img_ch=1)
-data_loader.get_test_data('final_data', img_h=96, img_w=128, img_ch=1)
+
+ROOT_PATH = str(pathlib.Path().absolute())
+DATA_STORAGE_PATH = '/data_storage/'
+STORAGE_PATH = ROOT_PATH + DATA_STORAGE_PATH
+
+DATASET_PATH = '/datasets/final_data'
+FINAL_DATASET_PATH = ROOT_PATH + DATASET_PATH
+
+TRAIN_PATH_IMAGES = FINAL_DATASET_PATH + '/train/images/'
+TEST_PATH_IMAGES = FINAL_DATASET_PATH + '/test/images/'
+VALI_PATH_IMAGES = FINAL_DATASET_PATH + '/validation/images/'
+
+TRAIN_PATH_MASKS = FINAL_DATASET_PATH + '/train/masks/'
+TEST_PATH_MASKS = FINAL_DATASET_PATH + '/test/masks/'
+VALI_PATH_MASKS = FINAL_DATASET_PATH + '/validation/masks/'
+
+data_loader.prepare_and_save_data(TRAIN_PATH_IMAGES, TRAIN_PATH_MASKS, type='train', img_h=96, img_w=128, img_ch=1)
+data_loader.prepare_and_save_data(TEST_PATH_IMAGES, TEST_PATH_MASKS, type='train', img_h=96, img_w=128, img_ch=1)
+# data_loader.get_test_data('final_data', img_h=96, img_w=128, img_ch=1)
--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,4 +1,6 @@
 import os
+from os import listdir
+from os.path import isfile, join
 import sys
 import pathlib
 
@@ -25,52 +27,44 @@ TEST_PATH_MASKS = FINAL_DATASET_PATH + '/test/masks/'
 VALI_PATH_MASKS = FINAL_DATASET_PATH + '/validation/masks/'
 
 
-def prepare_image_data(path, save_file_name, dataset_name, img_h=96, img_w=128, img_ch=1):
+def prepare_and_save_data(image_path, mask_path, type, dataset_name, img_h=96, img_w=128, img_ch=1):
     sys.stdout.flush()
 
-    file_ids = next(os.walk(path))[2]
-    data = np.zeros((len(file_ids), img_h, img_w, img_ch), dtype=np.uint8)
+    file_ids = next(os.walk(image_path))[2]
+    image_files_type = 'X_' + type
+    mask_files_type = 'Y_' + type
+
+    image_data = np.zeros((len(file_ids), img_h, img_w, img_ch), dtype=np.uint8)
+    mask_data = np.zeros((len(file_ids), img_h, img_w, 1), dtype=np.bool)
+    mask_sizes = []
 
     for n, id_ in tqdm(enumerate(file_ids), total=len(file_ids)):
         # Read image files iteratively
-        img = imread(path + id_)[:, :, :img_ch]
+        img = imread(image_path + id_)[:, :, :img_ch]
         img = resize(img, (img_h, img_w), mode='constant', preserve_range=True)
-        data[n] = img
+        image_data[n] = img
 
-    save_path = STORAGE_PATH + dataset_name + '/' + save_file_name
-    np.save(save_path, data)
-    print("{0}.npy has been saved at {1} ".format(save_file_name, STORAGE_PATH + dataset_name))
-
-
-def prepare_mask_data(path, save_file_name, dataset_name, img_h=96, img_w=128):
-    sys.stdout.flush()
-
-    file_ids = next(os.walk(path))[2]
-    data = np.zeros((len(file_ids), img_h, img_w, 1), dtype=np.bool)
-    mask_sizes = []
+    save_path = STORAGE_PATH + dataset_name + '/' + image_files_type
+    np.save(save_path, image_data)
+    print("{0}.npy has been saved at {1} ".format(image_files_type, STORAGE_PATH + dataset_name))
 
     for n, id_ in tqdm(enumerate(file_ids), total=len(file_ids)):
         # Read corresponding mask files iteratively
         mask = np.zeros((img_h, img_w, 1), dtype=np.bool)
 
-        mask_ = imread(path + id_)
+        mask_ = imread(mask_path + id_)
         mask_sizes.append([mask.shape[0], mask.shape[1]])
         # Expand individual mask dimensions
         mask_ = np.expand_dims(resize(mask_, (img_h, img_w), mode='constant', preserve_range=True), axis=-1)
 
         mask = np.maximum(mask, mask_)
-        data[n] = mask
+        mask_data[n] = mask
 
-    save_path = STORAGE_PATH + dataset_name + '/' + save_file_name
-    np.save(save_path, data)
-    print("{0}.npy has been saved at {1} ".format(save_file_name, STORAGE_PATH + dataset_name))
-
+    save_path = STORAGE_PATH + dataset_name + '/' + mask_files_type
+    np.save(save_path, mask_data)
+    print("{0}.npy has been saved at {0} ".format(mask_files_type, STORAGE_PATH + dataset_name))
     np.save(save_path + '_size', mask_sizes, allow_pickle=True)
-    print("{0}.npy has been saved at {1} ".format(save_file_name + '_size', STORAGE_PATH + dataset_name))
-
-def get_train_data(dataset_name, img_h=96, img_w=128, img_ch=1):
-    prepare_image_data(TRAIN_PATH_IMAGES, 'X_train', dataset_name, img_h=img_h, img_w=img_w, img_ch=img_ch)
-    prepare_mask_data(TRAIN_PATH_MASKS, 'Y_train', dataset_name, img_h=img_h, img_w=img_w)
+    print("{0}_size.npy has been saved at {0} ".format(mask_files_type, STORAGE_PATH + dataset_name))
 
 
 def load_train_data(dataset_name, is_dots_expanded=True, expand_size=1):
@@ -87,12 +81,6 @@ def load_train_data(dataset_name, is_dots_expanded=True, expand_size=1):
         'train_label_data': Y_train
     }
 
-
-def get_test_data(dataset_name, img_h=96, img_w=128, img_ch=1):
-    prepare_image_data(TEST_PATH_IMAGES, 'X_test', dataset_name, img_h=img_h, img_w=img_w, img_ch=img_ch)
-    prepare_mask_data(TEST_PATH_MASKS, 'Y_test', dataset_name, img_h=img_h, img_w=img_w)
-
-
 def load_test_data(dataset_name, is_dots_expanded=True, expand_size=1):
     file_path = STORAGE_PATH + dataset_name
 
@@ -106,11 +94,6 @@ def load_test_data(dataset_name, is_dots_expanded=True, expand_size=1):
         'test_data': X_test,
         'test_label_data': Y_test
     }
-
-
-def get_validation_data(dataset_name, img_h=96, img_w=128, img_ch=1):
-    prepare_image_data(VALI_PATH_IMAGES, 'X_val', dataset_name, img_h=img_h, img_w=img_w, img_ch=img_ch)
-    prepare_mask_data(VALI_PATH_MASKS, 'Y_val', dataset_name, img_h=img_h, img_w=img_w)
 
 def load_val_data(dataset_name, is_dots_expanded=True, expand_size=1):
     file_path = STORAGE_PATH + dataset_name
