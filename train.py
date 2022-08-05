121ffdb1a58130374611991d9531eac1c0ee0c20
lbbduy <baticsute@gmail.com>
2022-08-06 01:50:33 +0700

    - Unet models modification.

--- train.py
+++ train.py
@@ -4,14 +4,21 @@ from utils import unet_model
 from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
 
+import tensorflow as tf
 
 config = ConfigProto()
 config.gpu_options.allow_growth = True
 session = InteractiveSession(config=config)
-
+tf.keras.backend.set_image_data_format('channels_last')  # TF dimension ordering in this code
 
 train_data = data_loader.load_train_data('final_data', is_dots_expanded=False)
-test_data = data_loader.load_test_data('final_data', is_dots_expanded=False)
 #
-unet = unet_model.get_unet_model(img_h=192, img_w=256, img_ch=1)
-unet_model.train_model(model=unet, train_data=train_data, batch_size=8, n_epochs=100)
+unet = unet_model.get_unet_model_v2(img_h=192, img_w=256, img_ch=1, n_feature_maps=32)
+unet_model.train_model(
+    model=unet,
+    train_data=train_data,
+    batch_size=8,
+    n_epochs=500,
+    model_checkpoint_path='./model_checkpoints/model_unet_v2_checkpoint.h5',
+    patience=50
+)
--- utils/unet_model.py
+++ utils/unet_model.py
@@ -11,7 +11,7 @@ import tensorflow as tf
 
 tf.keras.backend.set_image_data_format('channels_last')  # TF dimension ordering in this code
 
-VALIDATION_SIZE_SPLIT = 0.05
+VALIDATION_SIZE_SPLIT = 0.2
 
 
 def dice_coef(target, prediction, axis=(0, 1), smooth=0.0001):
@@ -84,7 +84,7 @@ def get_unet_model_v2(img_h=96, img_w=128, img_ch=1, n_feature_maps=32):
         kernel_size=(3, 3),
         activation="relu",
         padding="same",
-        kernel_initializer="he_uniform"
+        kernel_initializer="he_normal"
     )
 
     params_trans = dict(
@@ -96,51 +96,69 @@ def get_unet_model_v2(img_h=96, img_w=128, img_ch=1, n_feature_maps=32):
     inputs = Input((img_h, img_w, img_ch))
 
     encodeA = Conv2D(name="encodeAa", filters=n_feature_maps, **params)(inputs)
+    encodeA = BatchNormalization()(encodeA)
     encodeA = Conv2D(name="encodeAb", filters=n_feature_maps, **params)(encodeA)
+    encodeA = BatchNormalization()(encodeA)
     poolA = MaxPooling2D(name="poolA", pool_size=(2, 2))(encodeA)
 
     encodeB = Conv2D(name="encodeBa", filters=n_feature_maps * 2, **params)(poolA)
+    encodeB = BatchNormalization()(encodeB)
     encodeB = Conv2D(name="encodeBb", filters=n_feature_maps * 2, **params)(encodeB)
+    encodeB = BatchNormalization()(encodeB)
     poolB = MaxPooling2D(name="poolB", pool_size=(2, 2))(encodeB)
 
     encodeC = Conv2D(name="encodeCa", filters=n_feature_maps * 4, **params)(poolB)
+    encodeC = BatchNormalization()(encodeC)
     encodeC = SpatialDropout2D(0.2)(encodeC)
     encodeC = Conv2D(name="encodeCb", filters=n_feature_maps * 4, **params)(encodeC)
+    encodeC = BatchNormalization()(encodeC)
     poolC = MaxPooling2D(name="poolC", pool_size=(2, 2))(encodeC)
 
     encodeD = Conv2D(name="encodeDa", filters=n_feature_maps * 8, **params)(poolC)
+    encodeD = BatchNormalization()(encodeD)
     encodeD = SpatialDropout2D(0.2)(encodeD)
     encodeD = Conv2D(name="encodeDb", filters=n_feature_maps * 8, **params)(encodeD)
+    encodeD = BatchNormalization()(encodeD)
     poolD = MaxPooling2D(name="poolD", pool_size=(2, 2))(encodeD)
 
     encodeE = Conv2D(name="encodeEa", filters=n_feature_maps * 16, **params)(poolD)
+    encodeE = BatchNormalization()(encodeE)
     encodeE = Conv2D(name="encodeEb", filters=n_feature_maps * 16, **params)(encodeE)
+    encodeE = BatchNormalization()(encodeE)
     up = Conv2DTranspose(name="transconvE", filters=n_feature_maps * 8, **params_trans)(encodeE)
 
     concatD = concatenate([up, encodeD], axis=concat_axis, name="concatD")
 
     decodeC = Conv2D(name="decodeCa", filters=n_feature_maps * 8, **params)(concatD)
+    decodeC = BatchNormalization()(decodeC)
     decodeC = Conv2D(name="decodeCb", filters=n_feature_maps * 8, **params)(decodeC)
+    decodeC = BatchNormalization()(decodeC)
 
     up = Conv2DTranspose(name="transconvC", filters=n_feature_maps * 4, **params_trans)(decodeC)
 
     concatC = concatenate([up, encodeC], axis=concat_axis, name="concatC")
 
     decodeB = Conv2D(name="decodeBa", filters=n_feature_maps * 4, **params)(concatC)
+    decodeB = BatchNormalization()(decodeB)
     decodeB = Conv2D(name="decodeBb", filters=n_feature_maps * 4, **params)(decodeB)
+    decodeB = BatchNormalization()(decodeB)
 
     up = Conv2DTranspose(name="transconvB", filters=n_feature_maps * 2, **params_trans)(decodeB)
     concatB = concatenate([up, encodeB], axis=concat_axis, name="concatB")
 
     decodeA = Conv2D(name="decodeAa", filters=n_feature_maps * 2, **params)(concatB)
+    decodeA = BatchNormalization()(decodeA)
     decodeA = Conv2D(name="decodeAb", filters=n_feature_maps * 2, **params)(decodeA)
+    decodeA = BatchNormalization()(decodeA)
 
     up = Conv2DTranspose(name="transconvA", filters=n_feature_maps, **params_trans)(decodeA)
 
     concatA = concatenate([up, encodeA], axis=concat_axis, name="concatA")
 
     convOut = Conv2D(name="convOuta", filters=n_feature_maps, **params)(concatA)
+    convOut = BatchNormalization()(convOut)
     convOut = Conv2D(name="convOutb", filters=n_feature_maps, **params)(convOut)
+    convOut = BatchNormalization()(convOut)
 
     prediction = Conv2D(
         name="PredictionMask",
@@ -160,35 +178,35 @@ def get_unet_model(img_h=96, img_w=128, img_ch=1):
 
     c1 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(s)
     c1 = BatchNormalization()(c1)
-    c1 = Dropout(0.1)(c1)
+    c1 = SpatialDropout2D(0.1)(c1)
     c1 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
     c1 = BatchNormalization()(c1)
     p1 = MaxPooling2D((2, 2))(c1)
 
     c2 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p1)
     c2 = BatchNormalization()(c2)
-    c2 = Dropout(0.1)(c2)
+    c2 = SpatialDropout2D(0.1)(c2)
     c2 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
     c2 = BatchNormalization()(c2)
     p2 = MaxPooling2D((2, 2))(c2)
 
     c3 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p2)
     c3 = BatchNormalization()(c3)
-    c3 = Dropout(0.2)(c3)
+    c3 = SpatialDropout2D(0.2)(c3)
     c3 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
     c3 = BatchNormalization()(c3)
     p3 = MaxPooling2D((2, 2))(c3)
 
     c4 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p3)
     c4 = BatchNormalization()(c4)
-    c4 = Dropout(0.2)(c4)
+    c4 = SpatialDropout2D(0.2)(c4)
     c4 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
     c4 = BatchNormalization()(c4)
     p4 = MaxPooling2D(pool_size=(2, 2))(c4)
 
     c5 = Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p4)
     c5 = BatchNormalization()(c5)
-    c5 = Dropout(0.3)(c5)
+    c5 = SpatialDropout2D(0.3)(c5)
     c5 = Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
     c5 = BatchNormalization()(c5)
 
@@ -196,7 +214,7 @@ def get_unet_model(img_h=96, img_w=128, img_ch=1):
     u6 = concatenate([u6, c4])
     c6 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u6)
     c6 = BatchNormalization()(c6)
-    c6 = Dropout(0.2)(c6)
+    c6 = SpatialDropout2D(0.2)(c6)
     c6 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c6)
     c6 = BatchNormalization()(c6)
 
@@ -204,7 +222,7 @@ def get_unet_model(img_h=96, img_w=128, img_ch=1):
     u7 = concatenate([u7, c3])
     c7 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u7)
     c7 = BatchNormalization()(c7)
-    c7 = Dropout(0.2)(c7)
+    c7 = SpatialDropout2D(0.2)(c7)
     c7 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c7)
     c7 = BatchNormalization()(c7)
 
@@ -212,7 +230,7 @@ def get_unet_model(img_h=96, img_w=128, img_ch=1):
     u8 = concatenate([u8, c2])
     c8 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u8)
     c8 = BatchNormalization()(c8)
-    c8 = Dropout(0.1)(c8)
+    c8 = SpatialDropout2D(0.1)(c8)
     c8 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c8)
     c8 = BatchNormalization()(c8)
 
@@ -220,7 +238,7 @@ def get_unet_model(img_h=96, img_w=128, img_ch=1):
     u9 = concatenate([u9, c1], axis=3)
     c9 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u9)
     c9 = BatchNormalization()(c9)
-    c9 = Dropout(0.1)(c9)
+    c9 = SpatialDropout2D(0.1)(c9)
     c9 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c9)
     c9 = BatchNormalization()(c9)
 
@@ -244,9 +262,9 @@ def get_model_logging(model_log_dir='./logs'):
     return TensorBoard(log_dir=model_log_dir, write_graph=True, write_images=True)
 
 
-def train_model(model, train_data, valid_data=None, batch_size=64, n_epochs=100):
-    model_checkpoint = get_model_checkpoint()
-    early_stopping = get_early_stopping()
+def train_model(model, train_data, valid_data=None, batch_size=64, n_epochs=100, model_checkpoint_path='./model_checkpoints/model_unet_checkpoint.h5', patience=10):
+    model_checkpoint = get_model_checkpoint(model_checkpoint_path=model_checkpoint_path)
+    early_stopping = get_early_stopping(patience=patience)
     tensorboards = get_model_logging()
 
     model.summary()
